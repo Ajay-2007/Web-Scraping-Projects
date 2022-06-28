1096b9492e813a89d58ad566b96f764001cb125c
Ajay-2007 <iit2017040@iiita.ac.in>
2022-06-29 02:34:05 +0530

    completed full webautomation

--- Playwright-JS/tests/5.full_webautomation.spec.js
+++ Playwright-JS/tests/5.full_webautomation.spec.js
@@ -18,6 +18,7 @@ test('register and login with playwright codegen', async ({page}) =>
     const products = page.locator(".card-body");
     const productName = "zara coat 3"
     
+    await page.pause();
     const count = await products.count();
     for (let i = 0; i < count; ++i) {
         if (await products.nth(i).locator("b").textContent() === productName) {
@@ -27,6 +28,62 @@ test('register and login with playwright codegen', async ({page}) =>
             break;
         }
     }
-    await page.pause();
+    // await page.pause();
+
+    // await page.waitForLoadState('networkidle');
+
+    await page.locator("[routerlink*='cart']").click();
+
+    await page.locator('div li').first().waitFor();
+
+    const bool = page.locator("h3:has-text('zara coat 3')").isVisible();
+
+    expect(bool).toBeTruthy();
+
+    // checkout button from css selector of button tag
+    // await page.locator("button[type='button']").click()
+    await page.locator('text=Checkout').click();
+    // delay will help us to slow the typing process with which the website will load the suggested country
+    await page.locator("[placeholder*='Country']").type("ind", {delay : 100});
+
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+
+    const optionsCount = await dropdown.locator("button").count();
+
+    for(let i = 0; i < optionsCount; ++i) {
+        const text = await dropdown.locator("button").nth(i).textContent();
+
+        if (text.trim() === "India") {
+            // click 
+            await dropdown.locator("button").nth(i).click();
+            break
+        }
+    }
+
+    const email = "username@gmail.com";
+
+    await expect(page.locator(".user__name input[type='text']")).toHaveText(email);
+
+    await page.locator(".action__submit").click();
+
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+
+    const orderIds = await page.locator(".em-spacer-1 .ng-star-inserted").allTextContents();
+    console.log(orderIds);
+
+    await page.locator('button[routerlink="/dashboard/myorders"]').click();
+
+    const tableRows = page.locator('table .ng-star-inserted');
+
+    const tableRowsCount = await tableRows.count();
+
+    console.log(orderIds[0]);
+    for (i = 0; i < tableRowsCount; ++i) {
+        const orderId = await tableRows.locator("table .ng-star-inserted th[scope='row']").nth(i).textContent();
+        if (orderIds[0] == orderId) {
+            console.log('order id found');
+        }
+    }
     
 })
